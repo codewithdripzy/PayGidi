c9bcb2e9407c9bf519ad0d80127fddeb4abad4c8
codewithdripzy <codewithdripzy@gmail.com>
2026-05-15 18:11:23 +0100

    fix: update database connection to force sslmode require

--- services/notification/config/database.go
+++ services/notification/config/database.go
@@ -10,7 +10,7 @@ import (
 
 func GetDBConnection() (*gorm.DB, error) {
 	dsn := fmt.Sprintf(
-		"host=%s port=%s user=%s dbname=%s password=%s sslmode=%s TimeZone=UTC",
+		"host=%s port=%s user=%s dbname=%s password=%s sslmode=require TimeZone=UTC",
 		constants.DB_HOST,
 		constants.DB_PORT,
 		constants.DB_USER,
